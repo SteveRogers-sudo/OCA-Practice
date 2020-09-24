0d0e52b73f38d5a2e6909dfd4cd4572973cfa778
Bishal Das <bdas1683@gmail.com>
2020-09-24 12:01:29 +0530

    D1-24th

---
+++ src/com/cg/ocaprep/Question12.java
@@ -0,0 +1,15 @@
+package com.cg.ocaprep;
+
+public class Question12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean myVal = false;
+	    if (myVal = true)
+	      for (int i = 0; i < 2; i++)
+	        System.out.println(i);
+	    else
+	      System.out.println("else");
+	}
+
+}
